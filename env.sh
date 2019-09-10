28d845717b8c83a524c698246affb980fad8ae5c
jllustres <43758742+jllustres@users.noreply.github.com>
2019-09-10 16:25:08 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,10 +3,10 @@
 user=$1
 pass=$2
 
-export http_proxy=http://$user:$pass@proxy.goya.local:8080
-export https_proxy=https://$user:$pass@proxy.goya.local:8080
+export http_proxy=http://$user:$pass@proxy:8080
+export https_proxy=https://$user:$pass@proxy:8080
 
-export ftp_proxy=ftp://$user:$pass@proxy.goya.local:8080/
+export ftp_proxy=ftp://$user:$pass@proxy:8080/
 
 
 echo "docker build --build-arg https_proxy="$https_proxy" --build-arg http_proxy="$http_proxy" -t $contenedor ."
